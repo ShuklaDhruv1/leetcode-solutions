d5573dae98a746dab2521ec68d29033cb0ae0faa
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-12 19:25:37 +0530

    Create GroupAnagrams.java

---
+++ medium/GroupAnagrams.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        System.out.println(groupAnagrams(words));
+    }
+}
