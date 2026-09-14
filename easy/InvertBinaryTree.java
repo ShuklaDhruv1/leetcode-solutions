86a76d1f68a8c2ec4d73fbc41ec3300d8b508c0f
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-14 14:44:56 +0530

    Create InvertBinaryTree.java

---
+++ easy/InvertBinaryTree.java
@@ -0,0 +1,62 @@
+public class InvertBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void printTree(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.val + " ");
+
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(4);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        System.out.print("Before Invert: ");
+        printTree(root);
+
+        invertTree(root);
+
+        System.out.print("\nAfter Invert: ");
+        printTree(root);
+    }
+}
