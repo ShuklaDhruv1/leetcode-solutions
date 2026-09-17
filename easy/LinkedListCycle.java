f4ffdab6ffa07bbe6447922749cf160e36c0511c
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-17 10:30:27 +0530

    Create LinkedListCycle.java

---
+++ easy/LinkedListCycle.java
@@ -0,0 +1,46 @@
+public class LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        // Create a cycle
+        node4.next = node2;
+
+        System.out.println("Cycle Found: " + hasCycle(node1));
+    }
+}
