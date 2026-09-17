f88620ccf4a48889238ecfc83215a3d248041558
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-17 10:27:07 +0530

    Create JumpGame.java

---
+++ medium/JumpGame.java
@@ -0,0 +1,31 @@
+public class JumpGame {
+
+    public static boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 1, 1, 4};
+
+        boolean result = canJump(nums);
+
+        System.out.println("Can Reach Last Index: " + result);
+    }
+}
