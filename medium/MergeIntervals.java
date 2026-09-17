a3a9725e715ed7e28678cdaeaa40b2384521dee0
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-17 10:27:49 +0530

    Create MergeIntervals.java

---
+++ medium/MergeIntervals.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+
+    public static int[][] merge(int[][] intervals) {
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                result.add(new int[]{start, end});
+
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        result.add(new int[]{start, end});
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {
+            {1, 3},
+            {2, 6},
+            {8, 10},
+            {15, 18}
+        };
+
+        int[][] result = merge(intervals);
+
+        System.out.println("Merged Intervals:");
+
+        for (int[] interval : result) {
+            System.out.println(
+                "[" + interval[0] + ", " + interval[1] + "]"
+            );
+        }
+    }
+}
