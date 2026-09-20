3d518fa1d2f4adb54767f9ad9954885d3f612429
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-20 19:21:40 +0530

    Create ExcelSheetColumnNumber.java

---
+++ easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,25 @@
+public class ExcelSheetColumnNumber {
+
+    public static int titleToNumber(String columnTitle) {
+
+        int result = 0;
+
+        for (char c : columnTitle.toCharArray()) {
+
+            int value = c - 'A' + 1;
+
+            result = result * 26 + value;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String column = "AB";
+
+        int result = titleToNumber(column);
+
+        System.out.println("Column Number: " + result);
+    }
+}
