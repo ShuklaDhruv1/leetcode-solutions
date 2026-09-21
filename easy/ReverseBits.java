a701d2fe6edd8255aadb55633b151a3a57fe63bc
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-21 19:41:18 +0530

    Create ReverseBits.java

---
+++ easy/ReverseBits.java
@@ -0,0 +1,25 @@
+public class ReverseBits {
+
+    public static int reverseBits(int n) {
+
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            result = (result << 1) | (n & 1);
+
+            n = n >>> 1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 43261596;
+
+        int result = reverseBits(n);
+
+        System.out.println("Reversed Bits: " + result);
+    }
+}
