0c71942c591e14f6da38fced3c227ebac11b684f
Dhruv Shukla <dhruvshukla617@gmail.com>
2026-09-22 20:21:18 +0530

    Create SameTree.java

---
+++ easy/SameTree.java
@@ -0,0 +1,48 @@
+public class SameTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isSameTree(
+            TreeNode p,
+            TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode tree1 = new TreeNode(1);
+        tree1.left = new TreeNode(2);
+        tree1.right = new TreeNode(3);
+
+        TreeNode tree2 = new TreeNode(1);
+        tree2.left = new TreeNode(2);
+        tree2.right = new TreeNode(3);
+
+        System.out.println(
+                "Are Trees Same: " +
+                isSameTree(tree1, tree2)
+        );
+    }
+}
